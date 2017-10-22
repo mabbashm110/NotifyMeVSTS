af94cf42ffe8bc29958929af62b174f615a46523
Sakina Shivji <shivjisakina@gmail.com>
2017-10-22 18:38:21 -0400

    tested walmart npm package and were liveeeeeeeee

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 var keys = require("./keys")    //This is keys.js reference
 var tempKeys = require("./tempKeys")
 var walmart = require('walmart')(tempKeys.walmartKeys);
+
+walmart.getItem(10449075).then(function(item) {
+    console.log(item.product.productName);
+});
--- tempKeys.js
+++ tempKeys.js
@@ -1,1 +1,1 @@
-exports.walmartKeys = {yyhdk4f353e4wht562g5heu4};
+exports.walmartKeys = "yyhdk4f353e4wht562g5heu4";
