2082b50ce4b463531ac2ec448e0ac29ea53450a9
MAbbas <mabbashm110@gmail.com>
2017-10-30 01:43:18 +0000

    Added node, express and handlebars

--- server.js
+++ server.js
@@ -2,6 +2,29 @@ var keys = require("./keys")    //This is keys.js reference
 var tempKeys = require("./tempKeys")
 var walmart = require('walmart')(tempKeys.walmartKeys);
 
-walmart.getItem(10449075).then(function(item) {
-    console.log(item.product.productName);
-});
+//Express
+var express = require('express')
+var exphbs  = require('express-handlebars');
+
+var app = express()
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    //getItem(Item number on Walmart)
+    walmart.getItem(173622323).then(function(item) {
+        //console.log(item.product.primaryImageUrl);
+        var imgAssets = item.product.imageAssets;
+        for (var i = 0; i < imgAssets.length; i++){
+            //console.log(imgAssets[i]);
+            var heroImg = imgAssets[i].versions.hero;
+            console.log(heroImg);
+            // res.setHeader("Content-Type", "text/html");
+            // res.write("<img src = '" + heroImg + "'/>");
+        }
+        res.render("home", {objWalmart: imgAssets});
+    });
+})
+
+
+app.listen(3000)
