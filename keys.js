7f60681dbf1e37951158c0da5845eeb54842ac60
MAbbas <mabbashm110@gmail.com>
2017-11-06 07:28:59 -0500

    Added Passport dependencies... 0.2, some work still pending

--- keys.js
+++ keys.js
@@ -1,1 +1,3 @@
 exports.walmartKeys = "yyhdk4f353e4wht562g5heu4";
+exports.tescoPKey = "7c7aa3bc076c447a8bda7a8dd36e4529";
+exports.tescoSKey = "d7b3cff1927a45c697af1867256aa35c";
--- server.js
+++ server.js
@@ -7,12 +7,44 @@ var express = require('express'),
     config = require('./config/app'), 
     app = express(),
     exphbs  = require('express-handlebars'),
-    Fitbit = require('fitbit'),
-    TescoAPI = require('tesco');
-    Tesco = new TescoAPI.default('apikey');;
 
+    port     = process.env.PORT || 8080,
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    flash    = require('connect-flash'),
+    morgan       = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser   = require('body-parser'),
+    session      = require('express-session'),
+    
+    configDB = require('./config/database.js'),
+
+    Fitbit = require('fitbit'),
+    TescoAPI = require('tesco'),
+    Tesco = new TescoAPI.default('apikey');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+//app.set('view engine', 'ejs'); // set up ejs for templating
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// required for passport
+app.use(session({ secret: 'iloveNotifyMe' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 //Walmart is home route
 app.get('/', function (req, res) {
@@ -29,12 +61,15 @@ app.get('/', function (req, res) {
         }
         res.render("home", {objWalmart: imgAssets});
     });
-})
+});
 
 app.get("/tesco", function(req,res){
     Tesco.search('Kellogs Cornflakes', { offset: 0, limit: 10 }, function(err, response) {
         if (err) console.log(err);
         console.log(response);
-})
+    })
+});
 
-app.listen(3000)
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port)
